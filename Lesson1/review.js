a6aeebed1ab3e500a91314eddd49aa828f624304
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-17 16:50:49 +0500

    Link the jquery library to the html file

---
+++ Lesson1/review.js
@@ -0,0 +1,3 @@
+// Biz jQuery darsini boshladik.
+// birinchi biz jQueyr kutubxonasini filega ko'chirib olib offline ishlatsa bo'ladi.
+// yoki cdn link orqali script taglari orqai html filelimizga ulab olsak bo'ladi.
