7ea870e3b3f8283c203ee484f4076a3323c77665
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-19 18:38:39 +0500

    effects

---
+++ Lesson3/js/index.js
@@ -0,0 +1,62 @@
+$(document).ready(() => {
+
+
+  // $('button').click(() => {
+  //   alert('Hello Foziljon!');
+  // });
+
+  // $('.contentHide').click(() => {
+  //   $('h1').hide();
+  // });
+
+  // $('.contentShow').click(() => {
+  //   $('h1').show();
+  // });
+
+  // $('.contentToggle').click(() => {
+  //   $('h1').toggle(1000);
+  // });
+
+  // =====================================
+
+  // $('.contentFadeIn').click(() => {
+  //   $('h1').fadeIn();
+  // })
+
+  // $('.contentFadeOut').click(() => {
+  //   $('h1').fadeOut();
+  // })
+
+  // $('.contentFadeToggle').click(() => {
+  //   $('h1').fadeToggle();
+  // })
+
+  // $('.contentFadeTo').click(() => {
+  //   $('h1').fadeTo(1000, .4);
+  // })
+
+  // =====================================
+
+  // $('.slideDown').click(() => {
+  //   $('h1').slideDown();
+  // })
+
+  // $('.slideUp').click(() => {
+  //   $('h1').slideUp();
+  // })
+
+  // $('.slideToggle').click(() => {
+  //   $('h1').slideToggle();
+  // })
+
+  // =====================================
+
+  // $('.boxBtn').click(() => {
+  //   $('.box').animate({
+  //     height: '100px',
+  //     width: '500px',
+  //   }, 2000)
+  // })
+
+
+});
---
+++ Lesson3/review.js
@@ -0,0 +1,24 @@
+// Mavzu: Effects
+
+// jQueryda  dollor belgisi ($) va jQuery bir xil kuchga ega.
+// Tanlab olishda $ belgisi o'rniga jQuery yozib ketsa bo'ladi.
+
+// Effects:
+// - hide();
+// - show();
+// - toggle(); bu bir vaqtning o'zida ham hide() va show() functions vazifasini bajaradi.
+// bular argumentida vaqt (ms)da qabul qiladi.
+
+// - fadeIn();
+// - fadeOut();
+// - fadetoggle(); bu bir vaqtning o'zida ham fadeIn() va fadeOut() functions vazifasini bajaradi.
+// bular argumentida vaqt (ms)da qabul qiladi.
+// - fadeTo(); buda o'zimiz vaqt va opacity belgilasak bo'ladi.
+
+// - slideDown();
+// - slideUp();
+// - slideToggle(); bu bir vaqtning o'zida ham slideDown() va slideUp() functions vazifasini bajaradi.
+// bular argumentida vaqt (ms)da qabul qiladi.
+
+// -animate({}, duration) animate argumentidagi object ichiga animation  bo'ladigan content stylelarini kiritiladi.
+// Yani shu content animation vaqtida nima holatga kelishini belgilab qo'yiladi.
