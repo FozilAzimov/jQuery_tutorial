74b7cb1e05552a684cc361c3246e0716c9280ae4
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-19 22:50:54 +0500

    text(), html(), val(), attr()

---
+++ Lesson4/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+
+  // var $title = $('h1').text();
+  // console.log($title);
+
+  // console.log($('h1').html());
+
+  // $('.btn').click(() => {
+  //   var $inputText = $('.inp').val();
+  //   var $type = $('.inp').attr('type');
+  //   console.log($inputText, " ", $type);
+  // });
+
+  // $('.btn').click(() => {
+  //   $('h1').text('Salom Dunyo!');
+  //   $('p').html('<b>Assalomu alaykum Foziljon Azimov!</b>');
+  //   $('.inp').val('Email');
+  //   $('.inp').attr({
+  //     'Foziljon': 'Azimov',
+  //     'placeholder': 'User Email',
+  //     'name': 'email'
+  //   });
+  // })
+
+});
---
+++ Lesson4/review.js
@@ -0,0 +1,6 @@
+// Mavzu: Element qiymatini olish;
+
+// text(), html(), val(), attr()  -  bular qiymat chiqaradi;
+
+// text('defaultText'), html('defaultText'), val('defaultValue'), attr({'typeName':'newValue'}) - bunda esa bor qiymatni yangilaydi;
+
