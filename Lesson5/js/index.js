e2817bc792c7d828ee0ef9c490e186a3238757d7
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-21 16:09:44 +0500

    img url push

--- Lesson5/js/index.js
+++ Lesson5/js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(() => {
 
-
+  $('input').attr('placeholder', 'url img push');
+  $('button').click(() => {
+    var $url = $('input').val();
+    $('img').attr({
+      'src': $url,
+      'alt': 'imgs',
+      'style': `
+      width: 500px;
+      display: block;
+      `
+    })
+  })
 
 });
