0e7ab21172c0832267acdd56c46e10ff6520afbb
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-21 23:36:26 +0500

    imgs and texts push

--- Lesson6/js/index.js
+++ Lesson6/js/index.js
@@ -1,11 +1,19 @@
 $(document).ready(() => {
 
-  $('button').click(() => {
+  $('.btn1').click(() => {
     var $value = $('input').val();
     $('.parent').append('<h1><a href="#">' + $value + '</a></h1>');
     $('input').val(null);
 
     // $('.parent').append('<img src=' + $value + '/>');
+  })
+
+  $('.btn2').click(() => {
+    $('.parent').remove();
+  })
+
+  $('.btn3').click(() => {
+    $('.parent').empty();
   })
 
 });
--- Lesson6/review.js
+++ Lesson6/review.js
@@ -1,3 +1,11 @@
 // Mavzu: img url push;
 
 // Asosiy qilingan ish inputdan value sini olib img tagining src attribute ga joylangan;
+
+// Asosiy functions:
+// append() - doimiy orqasidan qo'shadi.
+// prepend() - doimiy oldidan qo'shadi.
+// before() - content oldidan qo'shadi.
+// after() - content orqasidan qo'shadi.
+// empty() - content child larini o'chiradi.
+// remove() - contentni to'liq childlari bilan o'chiradi.
