23d19b0ecb7349b42f1f5ef009aa4bcaaa3703a8
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-22 22:03:31 +0500

    document size

---
+++ Lesson8/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+  // var innerWidth = $('body, html').innerWidth();
+  // var innerHeight = $('body, html').innerHeight();
+
+  // var outerWidth = $('body, html').outerWidth();
+  // var outerHeight = $('body, html').outerHeight();
+
+  // console.log(outerWidth, outerHeight);
+
+  // var koordinate = $('b').offset();
+  // var koordinate = $('b').position();
+  // console.log(koordinate);
+
+  // $(document).scroll(function () {
+  //   var scrolls = $('html, body').scrollTop();
+  //   console.log(scrolls);
+  // })
+
+})
---
+++ Lesson8/review.js
@@ -0,0 +1,11 @@
+// Mavzu: width, height, position, koordinates, scroll;
+
+// Asosiy functions:
+
+// - innerWidth(),
+// - innerHeight(),
+// - outerWidth(),
+// - outerHeight(),
+// - offset(),
+// - position(),
+// - scrollTop();
