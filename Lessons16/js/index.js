c3f24789526054b6920cb48cf209f2d005dc61fc
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-27 16:48:23 +0500

    jquery claviatura events

---
+++ Lessons16/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+  // $('.text').keydown(function () {
+  //   console.log('keyDown');
+  // })
+  // $('.text').keypress(function () {
+  //   console.log('keyPress');
+  // })
+  // $('.text').keyup(function () {
+  //   console.log('keyUp');
+  // })
+
+  $('.text').keypress(function (event) {
+    // console.log(event.currentTarget.value);
+    // console.log(event.target.value);
+    // console.log(event.key);
+    // console.log(event.which);
+    // console.log(event.which);
+  })
+
+  // $('.btn').click(function (event) {
+  //   event.preventDefault();
+  // })
+
+  // $('.box-1').click(function (event) {
+  //   console.log('Box-1');
+  //   event.stopPropagation();
+  // })
+  // $('.box-2').click(function (event) {
+  //   console.log('Box-2');
+  //   event.stopPropagation();
+  // })
+  // $('.box-3').click(function (event) {
+  //   console.log('Box-3');
+  //   event.stopPropagation();
+  // })
+
+});
---
+++ Lessons16/review.js
@@ -0,0 +1,13 @@
+// Mavzu: jQuery claviatura event;
+
+// Asosiy functions:
+
+// - keydown(),
+// - keypress(),
+// - keyup(),
+// - event,
+// - event.which,
+// - event.target,
+// - event.key,
+// - event.preventDefault(),
+// - event.stopPropagation(),
