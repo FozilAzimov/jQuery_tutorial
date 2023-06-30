15d4e48892d8fa1265cf88d2e3a34b03c5d78448
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-30 23:21:20 +0500

    ajax davomi

---
+++ Lessons20/js/index.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+
+  // $.ajax({
+  //   url: 'http://localhost:8081/api/v1/houses/list',
+  //   type: 'GET',
+  //   success: function (data, status) {
+  //     console.log(data, status);
+  //   },
+  //   error: function (error) {
+  //     console.log(error);
+  //   }
+  // })
+
+  // $.ajax({
+  //   url: 'http://localhost:8081/api/v1/houses/list',
+  //   type: 'GET'
+  // })
+  //   .done(function (data, status, jqXHR) {
+  //     console.log(data, status, jqXHR);
+  //   })
+  //   .fail(function (jqXHR, status, error) {
+  //     console.log(jqXHR, status, error);
+  //   }).always(function (data_jqXHR, status, jqXHR_error) {
+  //     console.log(data_jqXHR, status, jqXHR_error);
+  //   }) // asosiysi done va fail ishlatilishi yetarli;
+
+  // $.ajax({
+  //   url: 'http://localhost:8081/api/v1/houses/list',
+  //   type: 'POST',
+  //   data: {
+  //     success: true,
+  //     message: "House list",
+  //     data: [
+  //       {
+  //         id: 4,
+  //         name: "Muxabbat uyi",
+  //         description: "sdfasdfs",
+  //         user: {
+  //           id: 1,
+  //           firstname: "Foziljon",
+  //           lastname: "Azimov",
+  //           email: "azimovfoziljon0@gmail.com"
+  //         },
+  //         houseDetails: {
+  //           room: 20,
+  //           bath: 10,
+  //           garage: 4,
+  //           area: 1200,
+  //           beds: 20,
+  //           yearBuilt: 2023
+  //         },
+  //         price: 3000,
+  //         salePrice: 2000,
+  //         location: {
+  //           longitude: 50,
+  //           latitude: 50
+  //         },
+  //         address: "Tokio",
+  //         city: "Tokio shahri",
+  //         region: "Tokio",
+  //         country: "Yaponiya",
+  //         zipCode: "368229930",
+  //         attachments: [
+  //           {
+  //             imgPath: "https://i.pinimg.com/originals/97/87/6e/97876e02f357290c1b90aa05e620da68.jpg"
+  //           }
+  //         ],
+  //         category: null,
+  //         status: true,
+  //         houseComponentsDto: null,
+  //         homeAmenitiesDto: null
+  //       }
+  //     ]
+  //   }
+  // })
+  //   .done(function (data, status, jqXHR) {
+  //     console.log(data, status, jqXHR);
+  //   })
+  //   .fail(function (jqXHR, status, error) {
+  //     console.log(jqXHR, status, error);
+  //   }).always(function (data_jqXHR, status, jqXHR_error) {
+  //     console.log(data_jqXHR, status, jqXHR_error);
+  //   }) // asosiysi done va fail ishlatilishi yetarli;
+
+});
---
+++ Lessons20/review.js
@@ -0,0 +1,28 @@
+// Mavzu: AJAX davomi;
+
+// Asosiy functions:
+
+// $.ajax({
+// url: 'http://...',
+// type: 'GET'
+// })
+//   .done(functioon(data, status, jqXHR){
+//      console.log(data, status, jqXHR);
+//  })
+//   .fale(functioon(jqXHR, status, error){
+//     console.log(jqXHR, status, error);
+//  })
+
+// $.ajax({
+//   url: 'http://...',
+//   type: 'POST',
+//   data: {}
+// })
+//   .done(functioon(data, status, jqXHR){
+//     console.log(data, status, jqXHR);
+//   })
+//   .fale(functioon(jqXHR, status, error){
+//     console.log(jqXHR, status, error);
+//   })
+
+// AJAX ni ishlatilishida eng qulay sintaksis shu hisoblanadi.
