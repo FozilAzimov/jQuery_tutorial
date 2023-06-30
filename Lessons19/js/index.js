90f086ec0594b80279602f4c5cd9aba4ff5fdeee
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-30 23:17:54 +0500

    ajax

---
+++ Lessons19/js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+  // $('.wrapper').load('main.html');
+
+
+  // $.get('http://localhost:8081/api/v1/houses/list', function (data, status, option) {
+  //   if (data && status) console.log(data, status, option);
+  //   else console.log('Not Found');
+  // })
+
+
+  // $.post('http://localhost:8081/api/v1/houses/list',
+  //   {
+  //     success: true,
+  //     message: "House list",
+  //     data: [
+  //       {
+  //         id: 4,
+  //         name: "Muxabbat uyi",
+  //         description: "sdfasdfs",
+  //         user: {
+  //           id: 1,
+  //           firstname: "Foziljon",
+  //           lastname: "Azimov",
+  //           email: "azimovfoziljon0@gmail.com"
+  //         },
+  //         houseDetails: {
+  //           room: 20,
+  //           bath: 10,
+  //           garage: 4,
+  //           area: 1200,
+  //           beds: 20,
+  //           yearBuilt: 2023
+  //         },
+  //         price: 3000,
+  //         salePrice: 2000,
+  //         location: {
+  //           longitude: 50,
+  //           latitude: 50
+  //         },
+  //         address: "Tokio",
+  //         city: "Tokio shahri",
+  //         region: "Tokio",
+  //         country: "Yaponiya",
+  //         zipCode: "368229930",
+  //         attachments: [
+  //           {
+  //             imgPath: "https://i.pinimg.com/originals/97/87/6e/97876e02f357290c1b90aa05e620da68.jpg"
+  //           }
+  //         ],
+  //         category: null,
+  //         status: true,
+  //         houseComponentsDto: null,
+  //         homeAmenitiesDto: null
+  //       }
+  //     ]
+  //   },
+  //   function (data, status) {
+  //     console.log(data, status);
+  //   }
+  // )
+
+
+
+
+});
---
+++ Lessons19/review.js
@@ -0,0 +1,9 @@
+// Mavzu: AJAX;
+
+// AJAX - bu Asinxron JavaScript XML
+// Vazifasi sahifan yangilamasdan Web server va Browser o'rtasida data almashinuvini amalga oshiradi;
+
+// Asosiy functions:
+
+// - get('url', function(data, status)),
+// - post('url', {name:'Gulbashakar', age:'40'}, function(data, status));
